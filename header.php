bbfed072b7fe956aceeeacdcc8ca4b704965faa0
Natalia Kolesnikova <78445570+starnata78@users.noreply.github.com>
2021-06-20 10:42:00 -0400

    Header & Footer

---
+++ header.php
@@ -0,0 +1,16 @@
+<header id="header">
+    <div id="logo">Entertainment Hub</div>
+    <nav id="main-nav">
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Charts</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Awards</a></li>
+            <li><a href="#">Contests</a></li>
+            <li><a href="#">Trivia</a></li>
+            <li><a href="#">More</a></li>
+            <li><a href="#">Log-in</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
---
+++ index.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+    <title>Entertainment Hub</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<?php
+require_once 'header.php';
+?>
+<main id="main">
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+    <p> CONTENT</p>
+</main>
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
