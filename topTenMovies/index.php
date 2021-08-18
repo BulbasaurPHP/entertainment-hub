f823347e827e60539403f74ac89fcd6937a0cc46
Eric Wickham <ericwickham@hotmail.com>
2021-08-18 13:00:27 -0400

    Update index.php
    removed the voting once a day restriction to limit bugs

--- topTenMovies/index.php
+++ topTenMovies/index.php
@@ -12,34 +12,17 @@ $dbItem = $c->getitems(Database::getDb());
 //If sent this is what this equals
 
 if(isset($_POST['voteTopTen'])) {
-    if($_SESSION['voted'] = true) {
-        echo 'You have already voted! come back tomorrow.';
-    } else { 
-    $movieID = $_POST['topten'];
-    $db = Database::getDb();
-    $count = $c->voteTopTen($movieID, $db);
-    $_SESSION['voted'] = true;
-    header('Location:leaderboard.php');
-    }
+    
+        
+        $movieID = $_POST['topten'];
+        $db = Database::getDb();
+        $count = $c->voteTopTen($movieID, $db);
+        //$_SESSION['voted'] = true;
+        header('Location:leaderboard.php');
+    
 }
 
 
-
-
-/**
- *  Steps
- *  
- * Create Poll
- * 
- * Build Form Based on Database entries. every vote adds 1 to the entry's score
- *              based off entry id add 1 to what it is in vote table
- * 
- * Provide option of nominating a new movie to the poll if you don't like any of the options  
- *      Nominations come at the behest of voting
- * 
- * Display Leaderboard
- * 
- */
 
 ?>
 
@@ -76,9 +59,9 @@ if(isset($_POST['voteTopTen'])) {
         </form>
 </div>
 
-<!--
+<!-- Nomination Feature
 <h2>Didn't see anything you like? </h2>
-<button>Nominate a different movie</button>
- -->
+<a href="newNomination.php"><button>Nominate a different movie</button></a>
+-->
 </body>
 </html>
